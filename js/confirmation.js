b3edb8681cf034371885acd59768b6e44f3f2add
Geordinateur-manjaro <contact@georges-grignard.fr>
2021-05-31 08:36:22 +0200

    restructuration en deux dossier suite au conseil du sage mentor :)

--- js/confirmation.js
+++
@@ -1,3 +0,0 @@
-localStorage.clear();
-document.getElementById('listing').innerHTML = '<h3>Féliciation</h3><p>Votre commande à correctement été enregistrer au numéro <strong>' + url.searchParams.get("order") + '</strong>. <br>Vous devrez recevoir par email une confirmation de celle-ci, nous vous remercions pour votre confiance et nous esperons vous revoir très bientot sur Orinoco ';
-document.getElementById('listing').appendChild(btn('Retour à l\'accueil'));
